fd3c2333839d02ac8811fdce2faf17c86191f661
Ahmed kaafi <axmetabdilahi1@gmail.com>
2026-03-07 16:55:56 +0300

    firstExr

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import UserCard from "./UserCard";
+import { FullName } from "./UserCard";
+
+
+function App() {
+  
+  return (
+    <> 
+    
+    <UserCard />
+    <  FullName />
+     </>
+  )
+    
+  
+}
+
+export default App
---
+++ src/UserCard.jsx
@@ -0,0 +1,17 @@
+const UserCard = () => {
+    return (
+        <div className="user-card">
+           
+            <h2 className="name">Eng Caseyr</h2>
+            <p className="email">eng.caseyr@gmail.com</p>
+        </div>
+    );
+};
+
+export const FullName = () => {
+    return (
+        <div className="full-name">Ahmed abdillahi hussien</div>
+    );
+};
+
+export default UserCard;
