a929d2590be5defdef35bd08f261a2e8ead8f209
Ahmed kaafi <axmetabdilahi1@gmail.com>
2026-03-08 10:58:50 +0300

    prosExr3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import UserCard from "./UserCard";
 import { FullName } from "./UserCard";
 import Blog2 from "./Blog2";
+import PropsEx3 from "./PropsEx3";
 
 
 function App() {
+  
   
   return (
     <> 
@@ -11,7 +13,20 @@ function App() {
     {/* <UserCard />
     <  FullName /> */}
 
-    <Blog2 />
+    {/* <Blog2 /> */}
+
+<PropsEx3 name = "eng caseyr"
+          email =  "caseyr@gmail.com"
+                 
+ />
+<PropsEx3 name = "eng kaafi" 
+          email =  "kaafi7@gmail.com" />
+<PropsEx3  name = "eng mizki"
+         email =  "mizki@gmail.com" />
+<PropsEx3  name = "eng halimo"
+         email =  "gaabey@gmail.com"/>
+<PropsEx3  name = "eng warsame"
+         email =  "warsame@gmail.com"/>
      </>
   )
     
---
+++ src/PropsEx3.jsx
@@ -0,0 +1,13 @@
+const PropsEx3 = ({ name, email }) => {
+    return(
+<div className="props-ex3">
+
+<p>
+   name: {name}
+</p>
+<span> email: {email}</span>
+</div>
+)
+}
+
+export default PropsEx3;
